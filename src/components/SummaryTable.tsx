42929f6165e0628f058764a53d24870782f72a4d
Barbara Soares <barbarasamela90@gmail.com>
2024-01-09 15:48:09 -0300

    feat: finished the habit creation modal and day details popover and also customized the progress in the habit square

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -3,12 +3,12 @@ import { HabitDay } from "./HabitDay";
 
 const weekDAys = ["D", "S", "T", "Q", "Q", "S", "S"];
 
-const summaryDates = generateDatesFromYearBeginning()
+const summaryDates = generateDatesFromYearBeginning();
 
-const minimumSummaryDatesSize = 18 * 7
-const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+const minimumSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
-console.log(summaryDates)
+console.log(summaryDates);
 
 export function SummaryTable() {
   return (
@@ -27,19 +27,25 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date => {
-          return <HabitDay key={date.toString()} />  
+        {summaryDates.map((date) => {
+          return (
+            <HabitDay 
+              key={date.toString()} 
+              amount={4} 
+              completed={Math.round(Math.random()* 5)} 
+            />
+          )
         })}
 
-       {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, i) => {
-         return (
-          <div 
-            key={i} 
-            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
-          />
-        
-         )
-       })} 
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
   );
